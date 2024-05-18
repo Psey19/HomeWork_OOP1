b58c5c8c7f40bcbb2f891902d0abba0e995f717b
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-05-18 22:40:04 +0500

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,32 @@
+public abstract class Product {
+    private String name;
+    private int cost;
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Product(String name, int cost){
+        this.name = name;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product: " +
+                "name - '" + name + '\'' +
+                ", cost(rub.) - " + cost;
+    }
+}
