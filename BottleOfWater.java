a7f1d64a9d69ad90da558d0799b734dd00f90fec
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-05-18 22:40:54 +0500

    Create BottleOfWater.java

---
+++ BottleOfWater.java
@@ -0,0 +1,22 @@
+public class BottleOfWater extends Product{
+    private double volume;
+
+    public BottleOfWater(String name, int cost, double volume) {
+        super(name, cost);
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", volume(l) - " + volume;
+    }
+}
